f5701485755df85ea79808b5f0180fdfd40cf875
GadashJohn <fox5b21@gmail.com>
2022-02-09 20:32:42 +0300

    add solution from JavaSyntax task0302

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        //напишите тут ваш код
+    }
+}
