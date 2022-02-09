43fa05e53d5418bc6a2107158b2e8cfe4205aae1
GadashJohn <fox5b21@gmail.com>
2022-02-09 21:54:17 +0300

    add solution from JavaSyntax task0302

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -9,6 +9,19 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         String militaryCommissar = ", явитесь в военкомат";
-        //напишите тут ваш код
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя - ");
+        String name = scanner.nextLine();
+        System.out.println();
+        System.out.print("Введите возраст - ");
+        int x = scanner.nextInt();
+
+        if (x>=18 && x<=28){
+            System.out.println(name+militaryCommissar);
+        }
+
+
+
     }
 }
