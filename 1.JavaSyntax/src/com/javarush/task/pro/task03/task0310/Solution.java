89b6f81a543da07add6bf9e0f412a37f87281128
GadashJohn <fox5b21@gmail.com>
2022-02-11 01:17:47 +0300

    add solution from JavaSyntax task0310 - Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0310/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task03.task0310;
+
+import java.util.Scanner;
+
+/* 
+Через тернии...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+     //   String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
+        if( number<5){
+            System.out.println("число меньше 5");
+        }else if( number>5){
+            System.out.println("число больше 5");
+        }else{
+            System.out.println("число равно 5");
+        }
+//решено
+
+      //  System.out.println(result);
+    }
+}
