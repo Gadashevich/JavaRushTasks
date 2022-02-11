4c44dd3b654ed2d3f1b81a6f0692ab43334bda1c
GadashJohn <fox5b21@gmail.com>
2022-02-11 16:27:22 +0300

    add solution from JavaSyntax task0309 - Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task03.task0309;
+
+import java.util.Scanner;
+
+/* 
+Ищем максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int numberA = scanner.nextInt();
+//        int numberB = scanner.nextInt();
+//        if (numberA > numberB) {
+//            System.out.println(numberA);
+//        } else {
+//            System.out.println(numberB);
+//        }
+
+
+        int numberA = scanner.nextInt();
+        int numberB = scanner.nextInt();
+
+        int rez = numberA > numberB ? numberA : numberB;
+        System.out.println(rez);
+    }
+}
