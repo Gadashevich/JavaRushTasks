8eeefc0854acc2cb3a898f7e552704f4f44ff35b
GadashJohn <fox5b21@gmail.com>
2022-02-12 13:50:51 +0300

    add solution from JavaSyntax task0404 - Done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String b = "Б";
+        int x = 0;
+        int y = 0;
+
+
+        while(x<2){
+            System.out.println(b.repeat(20));
+            while (y<9) {
+            System.out.println("Б                  Б");
+            // while (y<)
+            y++;
+        }
+            x++;
+
+        }
+// можно нарисовать пробелы, но как обойтись без этого? Возможно нужно взять букву Б в цикл
+    }
+}
