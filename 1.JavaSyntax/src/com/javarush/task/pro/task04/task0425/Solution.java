23940c2343d1160156409886ca445fc30cb7a3db
GadashJohn <fox5b21@gmail.com>
2022-02-15 15:36:39 +0300

    add solution from JavaSyntax task0425 - 4 часть done

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -16,11 +16,9 @@ public class Solution {
         for (int i = 0; i <10 ; i++) {
             for (int j = 0; j <10 ; j++) {
                 if(i==9){
-                    System.out.print(bug);
-                } else if(j==3 && i<8){
+                    System.out.print(death);
+                } else if(j==3 && i<9){
                     System.out.print(hole);
-                } else if(j==3 && i==8){
-                    System.out.print(robot);
                 } else{
                     System.out.print(land);
                 }
