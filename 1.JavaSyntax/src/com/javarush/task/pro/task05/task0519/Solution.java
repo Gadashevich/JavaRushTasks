3afb01bb433c366ab1082b8421b0b790ccd1077d
GadashJohn <fox5b21@gmail.com>
2022-02-19 21:12:09 +0300

    add solution from JavaSyntax task0519 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task05.task0519;
+
+import java.util.Arrays;
+
+/* 
+Есть ли кто?
+*/
+
+public class Solution {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+       int[] copy =  Arrays.copyOf(array,array.length) ;
+       Arrays.sort(copy);
+        int i = Arrays.binarySearch(copy, element);
+        System.out.println(i >= 0 ? "true": "false");
+    }
+}
