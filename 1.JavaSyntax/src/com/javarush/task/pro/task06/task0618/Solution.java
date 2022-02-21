55912ec278d02f3e6aef7bc1ed8f55251632d47a
GadashJohn <fox5b21@gmail.com>
2022-02-21 23:12:02 +0300

    add solution from JavaSyntax task0618 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0618/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task06.task0618;
+
+/* 
+Злобный призрак
+*/
+
+public class Solution {
+    public static String material= "материальный";
+    public static String immaterial= "нематериальный";
+
+    public static void main(String[] args) {
+        System.out.println("Из болота вылез нематериальный монстр...");
+
+        switchSword(immaterial);
+    }
+
+   public static void switchSword(String arg){
+       System.out.println("Меч переключен в "+ arg + " режим.");
+   }
+
+
+}
