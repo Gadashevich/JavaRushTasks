95f00810c22a75feb75c64f790403a526cde3f95
GadashJohn <fox5b21@gmail.com>
2022-02-23 15:53:31 +0300

    add solution from JavaSyntax task0621 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    int i= Integer.MIN_VALUE;
+    double a = Double.MIN_VALUE;
+    byte x = Byte.MIN_VALUE;
+    short z = Short.MIN_VALUE;
+    long c = Long.MIN_VALUE;
+}
