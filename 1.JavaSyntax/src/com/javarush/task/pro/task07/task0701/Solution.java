11b740f086aebf35818261767f329c1730db9423
GadashJohn <fox5b21@gmail.com>
2022-02-23 17:10:20 +0300

    add solution from JavaSyntax task0701 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int ks;
+    long js;
+    char gs;
+    double fs;
+    short ds;
+    byte s;
+    boolean a;
+    float c;
+}
