e9997903099546170e511572940acfb733b095f6
GadashJohn <fox5b21@gmail.com>
2022-03-06 20:47:29 +0300

    add solution from JavaSyntax task1017 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+
+        Africa africa = new Africa(30);
+        Antarctica antarctica = new Antarctica(50);
+        Australia australia = new Australia(34);
+        Eurasia eurasia = new Eurasia(33);
+        NorthAmerica northAmerica = new NorthAmerica(80);
+        SouthAmerica southAmerica = new SouthAmerica(50);
+
+    }
+}
+
+
