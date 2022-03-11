5ed6b75a33e9969fa9e176539bb02217542af13a
GadashJohn <fox5b21@gmail.com>
2022-03-11 19:04:13 +0300

    add solution from JavaSyntax task1020 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Iphone.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task10.task1010;
+
+import java.util.Objects;
+
+/* 
+Два айфона
+*/
+
+public class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Iphone iphone = (Iphone) o;
+        return price == iphone.price && model.equals(iphone.model) && color.equals(iphone.color);
+    }
+
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+
+        System.out.println(iphone1.equals(iphone2));
+    }
+
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+
+    Keyboard keyboard = new Keyboard();
+    Monitor monitor = new Monitor();
+    Mouse mouse = new Mouse();
+    SystemUnit systemUnit = new SystemUnit();
+}
