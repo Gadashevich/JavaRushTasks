c0126a9b233a4a0ba9f5d89d32d5eca5d572766a
GadashJohn <fox5b21@gmail.com>
2022-03-26 00:23:10 +0300

    add solution from JavaSyntax task1117  - done

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,10 @@
+/* 
+Пакет нужен?
+*/
+package com.javarush.task.pro.task11.task1117;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Test package");
+    }
+}
