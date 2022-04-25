d8275a2c2c48103579e704072b16747e4bc320ea
GadashJohn <fox5b21@gmail.com>
2022-04-26 01:33:00 +0300

    add solution from JavaCore task1208 - done

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String x){
+
+    }
+    public static void print(int i){
+
+    }
+    public static void print(Object o){
+
+    }
+    public static void print(Boolean a){
+
+    }
+    public static void print(Character chars){
+
+    }
+}
