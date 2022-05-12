5fd56dda04995b8869ca03a6f3bccc4414bddf78
GadashJohn <fox5b21@gmail.com>
2022-05-12 21:48:01 +0300

    add solution from JavaCore task1414 - done

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task14.task1414;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+MovieFactory
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String string;
+        while((string = reader.readLine()) != null) {
+            Movie movie = MovieFactory.getMovie(string);
+            if(movie == null) {
+                break;
+            }
+            System.out.println(movie.getClass().getSimpleName());
+        }
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            return switch (key) {
+                case "soapOpera" -> new SoapOpera();
+                case "cartoon" -> new Cartoon();
+                case "thriller" -> new Thriller();
+                default -> null;
+            };
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie{
+
+    }
+    static class Thriller extends Movie{
+
+    }
+}
