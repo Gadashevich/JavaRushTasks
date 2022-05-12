e2bbce47b6d20ad0898be9de3d046987bfc666a8
GadashJohn <fox5b21@gmail.com>
2022-05-12 21:26:53 +0300

    add solution from JavaCore task1421 - done

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Singleton.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task14.task1421;
+
+public class Singleton {
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance(){
+    return instance;
+    }
+
+}
