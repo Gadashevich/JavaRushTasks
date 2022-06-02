0e2d6835a9f00856903000ae4ec945bc650f8091
GadashJohn <fox5b21@gmail.com>
2022-06-02 21:19:12 +0300

    add solution from JavaCore task1319 - done

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.*;
+/* 
+Писатель в файл с консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(reader.readLine()))) {
+            while (true) {
+                String string = reader.readLine();
+                writer.write(string + System.lineSeparator());
+                if (string.equalsIgnoreCase("exit")) {
+                    break;
+                }
+            }
+        }
+    }
+}
