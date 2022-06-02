fbd972a822f24ff0adcc59bea67019dddbbd9a5e
GadashJohn <fox5b21@gmail.com>
2022-06-02 21:30:11 +0300

    add solution from Game2048 2/18 - done

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,5 +2,11 @@ package com.javarush.games.game2048;
 
 import  com.javarush.engine.cell.*;
 
-public class Game2048 extends Game{
+public class Game2048 extends Game {
+    private static final int SIDE = 4;
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE,SIDE);
+    }
 }
