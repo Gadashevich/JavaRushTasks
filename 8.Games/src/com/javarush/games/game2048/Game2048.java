9b5c11dffff21ff199d87e03b0dfb2c4556494ae
GadashJohn <fox5b21@gmail.com>
2022-06-02 21:51:24 +0300

    add solution from Game2048 4/18 - done

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -10,10 +10,19 @@ public class Game2048 extends Game {
     public void initialize() {
         setScreenSize(SIDE,SIDE);
         createGame();
+        drawScene();
     }
 
     private void createGame(){
 
+    }
+
+    private void drawScene(){
+        for (int y = 0; y <SIDE ; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                setCellColor(x, y, Color.RED);
+            }
+        }
     }
 
 
