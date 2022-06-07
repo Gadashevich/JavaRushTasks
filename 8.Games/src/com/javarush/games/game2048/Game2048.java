ef27eb04ced1c942103cda042ce46eb136d8818f
GadashJohn <fox5b21@gmail.com>
2022-06-07 18:27:25 +0300

    add solution from Game2048 12/18 - done

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -120,15 +120,27 @@ public class Game2048 extends Game {
     }
 
     private void moveRight(){
-
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
     }
 
-    private void moveUp(){
-
+    private void moveUp() {
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
     }
 
     private void moveDown(){
-
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
     }
 
     private void rotateClockwise(){
