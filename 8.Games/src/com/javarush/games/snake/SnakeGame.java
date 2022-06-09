1daf37da8fea5b372e3f27f9835aa6bd1b5486af
GadashJohn <fox5b21@gmail.com>
2022-06-09 18:05:11 +0300

    add solution from GameSnake 10/20 - done

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -4,28 +4,25 @@ import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
 
-    public static  final int WIDTH = 15;
-    public static  final int HEIGHT = 15;
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
     private Snake snake;
-    private int turnDelay ;
+    private int turnDelay;
 
     @Override
     public void initialize() {
-        setScreenSize(WIDTH,HEIGHT);
+        setScreenSize(WIDTH, HEIGHT);
         createGame();
-    }
-
-    private void createGame() {
-        snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
         turnDelay = 300;
         setTurnTimer(turnDelay);
+
     }
 
     private void drawScene() {
-        for (int i = 0; i <WIDTH ; i++) {
+        for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
-                setCellValueEx(i,j,Color.DARKSEAGREEN,"");
+                setCellValueEx(i, j, Color.DARKSEAGREEN, "");
             }
         }
         snake.draw(this);
@@ -36,4 +33,10 @@ public class SnakeGame extends Game {
         snake.move();
         drawScene();
     }
+
+    private void createGame() {
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
+
+    }
+
 }
