620f8f2fec5695f746b91538e3c171fd82f26f12
GadashJohn <fox5b21@gmail.com>
2022-06-16 21:30:45 +0300

    add solution from JavaCore task1522 - Done

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task15.task1522;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Закрепляем паттерн Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static Planet thePlanet;
+
+    static {
+        readKeyFromConsoleAndInitPlanet();
+    }
+
+    public static void readKeyFromConsoleAndInitPlanet() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = reader.readLine();
+            switch (str) {
+                case Planet.MOON:
+                    thePlanet = Moon.getInstance();
+                    break;
+                case Planet.EARTH:
+                    thePlanet = Earth.getInstance();
+                    break;
+                case Planet.SUN:
+                    thePlanet = Sun.getInstance();
+                    break;
+                default:
+                    thePlanet = null;
+                    break;
+            }
+        } catch (IOException ignore) {
+        }
+
+    }
+}
