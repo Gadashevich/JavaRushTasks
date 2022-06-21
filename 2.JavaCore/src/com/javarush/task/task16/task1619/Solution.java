5ced55a2ace636d6c90bb5fee10e1bacd7ed71e2
GadashJohn <fox5b21@gmail.com>
2022-06-21 19:59:40 +0300

    add solution from javaCore task1619 - Done

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+    private static volatile boolean flag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        flag = true;
+    }
+
+    public static class TestThread implements Runnable {
+        public void run() {
+            while (!flag) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }
+    }
+}
