f48f41e5585b5674b90bc1d141c62df3ae11ff90
GadashJohn <fox5b21@gmail.com>
2022-08-30 21:24:08 +0300

    add solution from javaMultithreading task2113 - Done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Horse> getHorses() {
+        return this.horses;
+    }
+}
+
