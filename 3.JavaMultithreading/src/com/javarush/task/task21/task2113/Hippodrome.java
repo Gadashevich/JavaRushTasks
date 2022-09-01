3aa44d3c092c2d2dbb5f3ce2a159a2e5befd0b41
GadashJohn <fox5b21@gmail.com>
2022-09-01 23:06:23 +0300

    add solution from javaMultithreading task2113 - Done

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,16 +1,21 @@
 package com.javarush.task.task21.task2113;
 
-import lombok.Getter;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    @Getter
     private List<Horse> horses = new ArrayList<>();
 
-    public static void main(String[] args) {
+    Hippodrome(List<Horse> horses){
+        this.horses = horses;
     }
 
+    public static void main(String[] args) {
+
+    }
+
+    public List<Horse> getHorses() {
+        return this.horses;
+    }
 }
 
