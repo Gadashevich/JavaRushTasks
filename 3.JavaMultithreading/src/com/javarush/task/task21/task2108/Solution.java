0d933c38a203b0794cf09488e6088c5f952842c1
GadashJohn <fox5b21@gmail.com>
2022-09-02 08:18:18 +0300

    add solution from javaMultithreading task2108- Done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task21.task2108;
+
+/* 
+Клонирование растений
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = tree.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant {
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable {
+        private String[] branches;
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+
+
+        @Override
+        protected Tree clone() throws CloneNotSupportedException {
+            return new Tree(getName(), Arrays.copyOf(branches, branches.length));
+        }
+    }
+}
