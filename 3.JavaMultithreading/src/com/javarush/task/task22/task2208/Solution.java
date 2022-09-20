c9b729cb2e3ce1a6459f4d0310acf04fb42819d2
GadashJohn <fox5b21@gmail.com>
2022-09-20 18:49:42 +0300

    add solution from javaMultithreading task2208

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Ukraine");
+        map.put("city", "Kiev");
+        map.put("age", null);
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (entry.getValue() != null) {
+                builder.append(entry.getKey())
+                        .append(" = '")
+                        .append(entry.getValue())
+                        .append("' and ");
+            }
+        }
+
+        return builder.substring(0, builder.lastIndexOf("'")+1);
+    }
+}
