e87aca38716dcc1e6719623025ff292124a1a8be
GadashJohn <fox5b21@gmail.com>
2022-09-20 18:15:02 +0300

    add solution from javaMultithreading task2203

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null) {
+            throw new TooShortStringException();
+        }
+
+        String[] split = string.split("\t");
+
+        if (split.length < 3) {
+            throw new TooShortStringException();
+        }
+        return split[1];
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}
