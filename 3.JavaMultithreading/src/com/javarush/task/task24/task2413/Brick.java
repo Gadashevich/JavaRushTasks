18ed156c8f6ee86163ef59a2203efdbf92834a73
GadashJohn <fox5b21@gmail.com>
2022-10-09 22:44:18 +0300

    add solution from javaMultithreading task2413 - done

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,17 +1,35 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Класс для объекта "кирпич".
+ */
 public class Brick extends BaseObject {
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
 
-    public Brick(double x, double y, double radius) {
-        super(x,y,radius);
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
-    @Override
-    public void draw(Canvas canvas) {
 
+    /**
+     * Рисуем себя на холсте
+     */
+    @Override
+    void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'H');
     }
 
+    /**
+     * Ничего не делаем - кирпич неподвижен
+     */
     @Override
-    public void move() {
-
+    void move() {
+        //do nothing
     }
 }
