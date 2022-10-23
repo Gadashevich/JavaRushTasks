0b78a244f070b2fae8a493ce17ad2a8cfefdfa38
Евгений <fox5b21@gmail.com>
2022-10-23 20:38:46 +0300

    add solution from javaMultithreading task2909 - step 2/15

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -13,7 +13,6 @@ public class Human {
 
     protected int[] size;
 
-    protected boolean isSoldier;
 
     public static final int FIRST = 1;
     public static final int SECOND = 2;
@@ -30,8 +29,9 @@ public class Human {
         return bloodGroup;
     }
 
-    public Human(boolean isSoldier) {
-        this.isSoldier = isSoldier;
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
         this.id = nextId;
         nextId++;
     }
@@ -55,25 +55,22 @@ public class Human {
     public int getCourse() {
         return course;
     }
+
     public List<Human> getChildren() {
         return Collections.unmodifiableList(children);
     }
 
-    public void addChild(Human human){
+    public void addChild(Human human) {
         children.add(human);
     }
 
-    public void removeChild(Human human){
+    public void removeChild(Human human) {
         children.remove(human);
     }
 
-    public void live() {
-        if (isSoldier)
-            fight();
-    }
 
-    public void fight() {
-    }
+
+
 
     public int getId() {
         return id;
