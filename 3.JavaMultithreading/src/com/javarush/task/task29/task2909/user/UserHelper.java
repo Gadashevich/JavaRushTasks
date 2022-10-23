4ee8e98761267ff1c0cef73d30aed08d3456ff36
Евгений <fox5b21@gmail.com>
2022-10-23 23:06:34 +0300

    add solution from javaMultithreading task2909 - step 14/15

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
@@ -6,8 +6,7 @@ public class UserHelper {
     private User userAnya = new User("Аня", "Смирнова", 10);
     private User userRoma = new User("Рома", "Виноградов", 30);
 
-    private boolean isManAnya = false;
-    private boolean isManRoma = true;
+
 
     public void printUsers() {
         userAnya.printInfo();
@@ -30,7 +29,6 @@ public class UserHelper {
     }
 
     public String getBossName(User user) {
-        Work work = user.getWork();
-        return work.getBoss();
+        return user.getBoss();
     }
 }
