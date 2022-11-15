889b1ba941a7b7d273aeff9f2611bacf1ead6b97
GadashJohn <fox5b21@gmail.com>
2022-11-15 20:16:40 +0300

    add solution from javaMultithreading task3008 - 2/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,37 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String line;
+        try {
+            line = console.readLine();
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            line = readString();
+        }
+        return line;
+    }
+
+    public static int readInt(){
+        int number;
+        try{
+            number =Integer.parseInt(readString());
+
+        }catch (NumberFormatException e){
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            number = readInt();
+        }
+        return number;
+    }
+
 }
