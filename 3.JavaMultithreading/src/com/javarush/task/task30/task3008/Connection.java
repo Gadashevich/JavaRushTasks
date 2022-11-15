7801fa03ba61f78b562b23bac7bcffc944b42fa3
GadashJohn <fox5b21@gmail.com>
2022-11-15 20:36:15 +0300

    add solution from javaMultithreading task3008 - 5/22

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -1,12 +1,13 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketAddress;
 
-public class Connection {
+public class Connection implements Closeable {
 
     private final Socket socket;
     private final ObjectOutputStream out;
