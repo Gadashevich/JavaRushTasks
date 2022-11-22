5ca60e8e0f3d5e183f2ec9d8291d004561971c94
GadashJohn <fox5b21@gmail.com>
2022-11-22 19:16:58 +0300

    add solution from javaMultithreading task2503 -end

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2503/Column.java
@@ -0,0 +1,80 @@
+package com.javarush.task.task25.task2503;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public enum Column implements Columnable {
+    Customer("Customer"),
+    BankName("Bank Name"),
+    AccountNumber("Account Number"),
+    Amount("Available Amount");
+
+    private String columnName;
+
+    private static int[] realOrder;
+
+    private Column(String columnName) {
+        this.columnName = columnName;
+    }
+
+    /**
+     * Задает новый порядок отображения колонок, который хранится в массиве realOrder.
+     * realOrder[индекс в энуме] = порядок отображения; -1, если колонка не отображается.
+     *
+     * @param newOrder новая последовательность колонок, в которой они будут отображаться в таблице
+     * @throws IllegalArgumentException при дубликате колонки
+     */
+    public static void configureColumns(Column... newOrder) {
+        realOrder = new int[values().length];
+        for (Column column : values()) {
+            realOrder[column.ordinal()] = -1;
+            boolean isFound = false;
+
+            for (int i = 0; i < newOrder.length; i++) {
+                if (column == newOrder[i]) {
+                    if (isFound) {
+                        throw new IllegalArgumentException("Column '" + column.columnName + "' is already configured.");
+                    }
+                    realOrder[column.ordinal()] = i;
+                    isFound = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * Вычисляет и возвращает список отображаемых колонок в сконфигурированом порядке (см. метод configureColumns)
+     * Используется поле realOrder.
+     *
+     * @return список колонок
+     */
+    public static List<Column> getVisibleColumns() {
+        List<Column> result = new LinkedList<>();
+        for (int i = 0; i < values().length; i++) {
+            result.add(null);
+        }
+        for (int i = 0; i < values().length; i++) {
+            if (realOrder[i] != -1) {
+                result.set(realOrder[i], values()[i]);
+            }
+        }
+        result.removeAll(Collections.singleton(null));
+        return result;
+    }
+
+    @Override
+    public String getColumnName() {
+        return columnName;
+    }
+
+    @Override
+    public boolean isShown() {
+        return realOrder[ordinal()] >= 0;
+    }
+
+    @Override
+    public void hide() {
+        realOrder[ordinal()] = -1;
+    }
+}
