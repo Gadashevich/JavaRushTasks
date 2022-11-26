848f0bb2cfae04ebdcbb239c0595557ee5929f00
GadashJohn <fox5b21@gmail.com>
2022-11-27 02:39:25 +0300

    add solution from javaMultithreading task3008 -18/32

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task30.task3008.client;
+
+public class BotClient extends Client {
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        return "date_bot_" + (int) (Math.random() * 100);
+    }
+
+    public static void main(String[] args) {
+        new BotClient().run();
+    }
+
+    public class BotSocketThread extends SocketThread {
+
+    }
+}
