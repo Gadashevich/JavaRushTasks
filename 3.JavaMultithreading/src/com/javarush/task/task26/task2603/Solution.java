11459079b8c3bd203f2186477499acbcf49c5f92
GadashJohn <fox5b21@gmail.com>
2022-11-27 22:58:43 +0300

    add solution from javaMultithreading task2603 - done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task26.task2603;
+
+import java.util.Comparator;
+
+/* 
+Убежденному убеждать других не трудно
+*/
+
+public class Solution {
+    public static class CustomizedComparator<T> implements Comparator<T> {
+        private Comparator<T>[] comparators;
+
+        @SafeVarargs
+        public CustomizedComparator(Comparator<T>... comparators) {
+            this.comparators = comparators;
+        }
+
+        @Override
+        public int compare(T o1, T o2) {
+            for (Comparator<T> comparator : comparators) {
+                int compare = comparator.compare(o1, o2);
+                if(compare != 0){
+                    return compare;
+                }
+            }
+            return 0;
+        }
+    }
+
+
+    public static void main(String... args) {
+
+    }
+}
