af62f2c6fc62f49f3d4504b074649f67ccf05557
GadashJohn <fox5b21@gmail.com>
2022-11-29 19:47:02 +0300

    add solution from javaMultithreading task2612 - done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2612/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task26.task2612;
+
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/* 
+Весь мир играет комедию
+*/
+
+public class Solution {
+    private Lock lock = new ReentrantLock();
+
+    public void someMethod() {
+        if (lock.tryLock()) {
+            try {
+                actionIfLockIsFree();
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            actionIfLockIsBusy();
+        }
+
+    }
+
+    public void actionIfLockIsFree() {
+    }
+
+    public void actionIfLockIsBusy() {
+    }
+}
