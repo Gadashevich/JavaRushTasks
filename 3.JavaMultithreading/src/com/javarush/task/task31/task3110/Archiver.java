f56ba5661b43ff9ecbc8280368de423c0a63ae6e
GadashJohn <fox5b21@gmail.com>
2022-12-15 01:04:55 +0300

    add solution from javaMultithreading task3110 - Archiver 8/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,23 +1,35 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT);
 
-        System.out.print("Введите полный путь архива с именем: ");
-        BufferedReader pathArchive = new BufferedReader(new InputStreamReader(System.in));
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathArchive.readLine()));
-        System.out.println();
-        System.out.print("Введите путь к файлу который нужно архивировать: ");
-        BufferedReader pathFile = new BufferedReader(new InputStreamReader(System.in));
-        zipFileManager.createZip(Paths.get(pathFile.readLine()));
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+        //вопрос
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
