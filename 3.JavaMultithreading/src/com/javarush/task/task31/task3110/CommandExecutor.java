804e9b4080701f908e2df715c57331daef3a439f
GadashJohn <fox5b21@gmail.com>
2022-12-15 00:38:09 +0300

    add solution from javaMultithreading task3110 - Archiver 6/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -1,28 +1,26 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import com.javarush.task.task31.task3110.command.*;
-
 public class CommandExecutor {
-    private static final Map<Operation, Command> ALL_KNOWN_COMMANDS_MAP = new HashMap<>();
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
 
     static {
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.ADD, new ZipAddCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.CONTENT, new ZipContentCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.CREATE, new ZipCreateCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXTRACT, new ZipExtractCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.REMOVE, new ZipRemoveCommand());
-        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXIT, new ExitCommand());
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
 
     private CommandExecutor() {
     }
 
-    public static void execute(Operation operation) throws Exception{
-        ALL_KNOWN_COMMANDS_MAP.get(operation).execute();
+    public static void execute(Operation operation) throws Exception {
+        allKnownCommandsMap.get(operation).execute();
     }
-
-
 }
