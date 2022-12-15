48d0b92b203ae690fd2d8a7d816c4fbdfc8178a4
GadashJohn <fox5b21@gmail.com>
2022-12-15 23:53:16 +0300

    add solution from javaMultithreading task3110 - Archiver 11/18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,8 +1,15 @@
 package com.javarush.task.task31.task3110.command;
 
-public abstract class ZipCommand implements Command {
-    @Override
-    public void execute() throws Exception {
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
 
+import java.nio.file.Paths;
+
+public abstract class ZipCommand implements Command {
+
+
+    public ZipFileManager getZipFileManager() throws  Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива: ");
+        return new ZipFileManager(Paths.get(ConsoleHelper.readString()));
     }
 }
