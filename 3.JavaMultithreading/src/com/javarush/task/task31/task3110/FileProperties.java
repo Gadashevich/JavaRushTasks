c3337765ff39465ce2d2daa04f910e38caa087de
GadashJohn <fox5b21@gmail.com>
2022-12-16 23:50:06 +0300

    add solution from javaMultithreading task3110 - Archiver 12/18

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task31.task3110;
+
+public class FileProperties {
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+
+    public long getCompressionRatio() {
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
+        return builder.toString();
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -10,17 +10,19 @@ import java.nio.file.Paths;
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-        try{
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
 
-        ConsoleHelper.writeMessage("Создание архива.");
-        ZipFileManager zipFileManager = getZipFileManager();
-        ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации: ");
-        Path path = Paths.get(ConsoleHelper.readString());
-        zipFileManager.createZip(path);
-        ConsoleHelper.writeMessage("Архив создан");
-        } catch (PathIsNotFoundException e){
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+
+            ConsoleHelper.writeMessage("Архив создан.");
+
+        } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
         }
     }
-
 }
