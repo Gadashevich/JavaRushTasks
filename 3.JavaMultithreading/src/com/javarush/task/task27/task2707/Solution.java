3463c207b74d9f9ca86f00a8f63287aed7a30037
GadashJohn <fox5b21@gmail.com>
2022-12-25 22:04:49 +0300

    add solution from javaMultithreading task2707 - done;

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task27.task2707;
+
+/* 
+Определяем порядок захвата монитора
+*/
+
+public class Solution {
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            synchronized (obj2) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
+    }
+
+    public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
+        synchronized (o1) {
+            Thread threadOne = new Thread(() -> {
+                synchronized (o2) {
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            });
+            new Thread(() -> solution.someMethodWithSynchronizedBlocks(o1, o2)).start();
+            threadOne.start();
+
+            Thread.sleep(50);
+            return threadOne.getState() != Thread.State.BLOCKED;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Solution solution = new Solution();
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+
+        System.out.println(isLockOrderNormal(solution, o1, o2));
+    }
+}
