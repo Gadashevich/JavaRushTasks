a50ee2e828516aae2a49e17f063b7b3c70e351ae
GadashJohn <fox5b21@gmail.com>
2023-01-10 19:47:30 +0300

    add solution from javaMultithreading task2807 - done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2807/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task28.task2807;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/* 
+Знакомство с ThreadPoolExecutor
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        LinkedBlockingQueue<Runnable> linkedBlockingQueue = new LinkedBlockingQueue<>();
+
+        for (int i = 1; i <= 10; i++) {
+            final int index = i;
+            linkedBlockingQueue.add(() -> doExpensiveOperation(index));
+        }
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,5,1000,TimeUnit.MILLISECONDS,linkedBlockingQueue);
+        threadPoolExecutor.prestartAllCoreThreads();
+        threadPoolExecutor.shutdown();
+        threadPoolExecutor.awaitTermination(5,TimeUnit.SECONDS);
+
+
+        /* Example output
+pool-1-thread-2, localId=2
+pool-1-thread-3, localId=3
+pool-1-thread-1, localId=1
+pool-1-thread-3, localId=5
+pool-1-thread-2, localId=4
+pool-1-thread-3, localId=7
+pool-1-thread-1, localId=6
+pool-1-thread-3, localId=9
+pool-1-thread-2, localId=8
+pool-1-thread-1, localId=10
+         */
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
