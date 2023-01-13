21cabfffca84027fea5ffbfba718d29ae1bd715c
GadashJohn <fox5b21@gmail.com>
2023-01-14 01:33:30 +0300

    add solution from javaMultithreading task3209 - 15/18

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.UndoListener;
+
+import javax.swing.text.AbstractDocument;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -31,4 +35,16 @@ public class Controller {
     public void exit() {
         System.exit(0);
     }
+
+
+    public void resetDocument(){
+        UndoListener undoListener = view.getUndoListener();
+        if (document != null) {
+            document.removeUndoableEditListener(undoListener);
+        }
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(undoListener);
+        view.update();
+    }
+
 }
