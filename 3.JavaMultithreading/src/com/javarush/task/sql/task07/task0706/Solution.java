f1b3b7b45b1420f535d3f4931cecc69bb6b4fb01
GadashJohn <fox5b21@gmail.com>
2023-02-12 22:07:08 +0300

    add solution from JDBC task0706 - done

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0706/Solution.java
@@ -9,7 +9,16 @@ task0706
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from employee limit 1");
+        ){
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount() ; i++) {
+                System.out.println(metaData.getColumnName(i) +"(" + metaData.getColumnTypeName(i)+")");
+            }
+
+        }
 
     }
 }
