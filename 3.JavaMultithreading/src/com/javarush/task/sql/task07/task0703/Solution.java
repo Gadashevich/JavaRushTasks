cda625cb5f6581f5186bbf844a76960d9779e3c9
GadashJohn <fox5b21@gmail.com>
2023-02-12 21:29:11 +0300

    add solution from JDBC task0703 - done

--- 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0703/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0703/Solution.java
@@ -9,7 +9,18 @@ task0703
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
+        ) {
+            System.out.println("id "+ " name"+" age");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                System.out.println(id +" " + name +" "+ age);
+            }
+        }
 
     }
 }
