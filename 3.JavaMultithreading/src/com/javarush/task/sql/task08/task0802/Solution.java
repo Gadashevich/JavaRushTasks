96d37003e025427bc56b12b168f59f62c5fcf66e
GadashJohn <fox5b21@gmail.com>
2023-02-12 22:57:16 +0300

    add solution from JDBC task0802 - done

--- 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0802/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task08/task0802/Solution.java
@@ -9,7 +9,17 @@ import java.sql.*;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+        try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
+            statement.executeUpdate("UPDATE employee set salary = salary + 2000 where name = 'Diego'");
+            statement.executeUpdate("UPDATE employee set salary = salary + 500 where name = 'Amigo'");
+            connection.commit();
+        } catch (SQLException e) {
+                connection.rollback();
+        } finally {
+            connection.close();
+        }
 
     }
 }
