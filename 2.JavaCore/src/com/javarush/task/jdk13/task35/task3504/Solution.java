939506f01695c31e4c9ea524e79431300cb5eb25
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-10-19 23:12:29 +0300

    done | 3504

--- 2.JavaCore/src/com/javarush/task/jdk13/task35/task3504/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3504/Solution.java
@@ -7,31 +7,30 @@ import java.util.LinkedHashMap;
 Простой generics
 */
 
-public class Solution {
-    private HashMap map;
+public class Solution <T extends HashMap<?,?>>{
+    private T map;
 
-
-
-    public Solution(HashMap map) {
+    public Solution(T map) {
         this.map = map;
     }
 
-    public HashMap getMap() {
+
+    public T getMap() {
         return map;
     }
 
     public static void main(String[] args) {
         HashMap<String, Integer> hashMap = new HashMap<>();
         hashMap.put("string", 4);
-        Solution solution = new Solution(hashMap);
+        Solution<?> solution = new Solution<>(hashMap);
         HashMap mapFromSolution = solution.getMap();
         System.out.println(mapFromSolution.getClass());
 
 
-        LinkedHashMap<Solution, Solution> hashMap2 = new LinkedHashMap<>();
+        LinkedHashMap<Solution<?>, Solution<?>> hashMap2 = new LinkedHashMap<>();
         hashMap2.put(solution, solution);
-        Solution solution2 = new Solution(hashMap2);
-        LinkedHashMap mapFromSolution2 = (LinkedHashMap) solution2.getMap();   //need to cast  :(
+        Solution<?> solution2 = new Solution<>(hashMap2);
+        LinkedHashMap<?,?> mapFromSolution2 = (LinkedHashMap<?,?>) solution2.getMap();   //need to cast  :(
         System.out.println(mapFromSolution2.getClass());
     }
 }
