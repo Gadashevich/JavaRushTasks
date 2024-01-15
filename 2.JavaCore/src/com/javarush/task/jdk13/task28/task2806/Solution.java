badc8cc9c7f5cdaf34ede0e5d870a8a88721bef2
Evgeniy Gadashevich <fox5b21@gmail.com>
2024-01-16 01:19:57 +0300

    done | task2806

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2806/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.jdk13.task28.task2806;
+
+import java.util.*;
+import java.util.concurrent.FutureTask;
+
+/* 
+Параллельный парсинг
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            lines.add(scanner.nextLine());
+        }
+
+        Map<Thread, ParseLinkTask> tasks = new HashMap<>();
+        for (String line : lines) {
+            ParseLinkTask parseLinkTask = new ParseLinkTask(line);
+            Thread thread = new Thread(new FutureTask<>(parseLinkTask) );
+            tasks.put(thread, parseLinkTask);
+            thread.start();
+        }
+
+        for (Map.Entry<Thread, ParseLinkTask> entry : tasks.entrySet()) {
+            entry.getKey().join();
+            System.out.println(entry.getValue().call());
+        }
+
+    }
+}
