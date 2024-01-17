90eb43bfb382f2114d09312447d3cb9cddbf6cb4
Evgeniy Gadashevich <fox5b21@gmail.com>
2024-01-18 00:52:36 +0300

    done | task2809

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2809/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.jdk13.task28.task2809;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/* 
+Многонитиевость в одной нити
+*/
+
+public class Solution {
+    public static ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+
+    public static void main(String[] args) throws Exception {
+        List<Future<String>> futures = new ArrayList<>();
+        List<String> instruments = List.of("скрипка", "тромбон", "бубенцы", "контрабас");
+        for (String instrument : instruments) {
+            futures.add(startPlaying(instrument));
+        }
+
+        System.out.println("Успели доиграть: " + completeConcert(futures));
+        executorService.shutdown();
+    }
+
+    public static Future<String> startPlaying(String name) {
+        //FutureTask<String> future = new FutureTask<>(new MusicalInstrument(name));
+        //new Thread(future).start();
+        //return future;
+
+        return executorService.submit(new MusicalInstrument(name));
+    }
+
+    public static List<String> completeConcert(List<Future<String>> futures) {
+        List<String> result = new ArrayList<>();
+        for (Future<String> future : futures) {
+            try {
+                result.add(future.get(800, TimeUnit.MILLISECONDS));
+            } catch (Exception e) {
+                future.cancel(true);
+            }
+        }
+        return result;
+    }
+}
