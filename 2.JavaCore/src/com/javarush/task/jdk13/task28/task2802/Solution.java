c7d6e1fa71dad15f6852f4261edee9c598da7f11
Evgeniy Gadashevich <fox5b21@gmail.com>
2024-01-17 09:56:46 +0300

    done | task2802

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2802/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.jdk13.task28.task2802;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/* 
+ThreadLocalRandom
+*/
+
+public class Solution {
+    public static int getRandomIntegerBetweenNumbers(int from, int to) {
+        return ThreadLocalRandom.current().nextInt(from,to);
+    }
+
+    public static double getRandomDouble() {
+
+        return ThreadLocalRandom.current().nextDouble();
+    }
+
+    public static long getRandomLongBetween0AndN(long n) {
+        return ThreadLocalRandom.current().nextLong(n);
+    }
+
+    public static void main(String[] args) {
+    }
+}
