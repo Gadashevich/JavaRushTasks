23ff0a849b7ecb0b33414e52dd654a19180476eb
Evgeniy Gadashevich <fox5b21@gmail.com>
2024-01-19 10:56:16 +0300

    done | task2813

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2813/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.jdk13.task28.task2813;
+
+import java.util.Collections;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+/* 
+Кеширование нитей
+*/
+
+public class Solution {
+
+    public static BlockingQueue<String> queue = new ArrayBlockingQueue<>(32);
+    public static ExecutorService executorService;
+
+    public static void main(String[] args) throws Exception {
+        executorService = Executors.newCachedThreadPool();
+
+        submitProducers();
+        submitConsumers();
+
+        executorService.shutdownNow();
+    }
+
+    public static void submitProducers() {
+        IntStream.range(0,100).forEach(i -> executorService.submit(new Producer(String.valueOf(i))));
+
+    }
+
+    public static void submitConsumers() {
+        Consumer consumer = new Consumer();
+        IntStream.range(0,100).forEach(i -> executorService.submit(consumer));
+
+    }
+}
