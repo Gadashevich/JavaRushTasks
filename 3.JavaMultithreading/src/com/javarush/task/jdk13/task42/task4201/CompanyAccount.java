5a661d37dc23d9b0096123a1096b7e8bec4cecfc
Evgeniy Gadashevich <fox5b21@gmail.com>
2024-02-04 02:12:02 +0300

    done | task4201

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4201/CompanyAccount.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task42.task4201;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CompanyAccount {
+    private AtomicInteger totalMoney;
+
+    public CompanyAccount(int totalMoney) {
+        this.totalMoney = new AtomicInteger(totalMoney);
+    }
+
+    public int getTotalMoney() {
+        return totalMoney.get();
+    }
+
+    public void addMoney(int money) {
+        totalMoney.getAndAdd(money);
+    }
+}
