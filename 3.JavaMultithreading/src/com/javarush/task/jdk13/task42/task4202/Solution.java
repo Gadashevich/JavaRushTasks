7eb404d2d236807a653e1cb544ada49a46330ee2
Evgeniy Gadashevich <fox5b21@gmail.com>
2024-02-04 02:12:42 +0300

    done | task4202

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4202/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.jdk13.task42.task4202;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/* 
+Кто первый встал, того и тапки
+*/
+
+public class Solution {
+    private static final int COUNT = 5;
+    private static final int TIMEOUT = 10;
+    private static AtomicBoolean isHit = new AtomicBoolean();   // по умолчанию false
+
+    public static void main(final String[] arguments) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(COUNT);
+
+        for (int i = 1; i <= COUNT; i++) {
+            Solution.Shooter shooter = new Solution.Shooter(i);
+            executor.submit(shooter);
+        }
+
+        executor.awaitTermination(TIMEOUT, TimeUnit.MILLISECONDS);
+
+        executor.shutdown();
+    }
+
+    static class Shooter implements Runnable {
+        private int id;
+
+        public Shooter(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Did someone hit? " + isHit);
+            if (isHit.compareAndSet(false,true)) {
+                System.out.println("The shooter #" + id + " hit!");
+                System.out.println("Did someone hit? " + isHit);
+            }
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task42/task4204/Key.java
@@ -0,0 +1,23 @@
+package com.javarush.task.jdk13.task42.task4204;
+
+import java.util.Random;
+
+public class Key {
+    private static final Random random = new Random();
+    private final int intField = random.nextInt(5) + 1;
+
+    public int getIntField() {
+        double random1 = Math.random();
+        return intField;
+    }
+
+    @Override
+    public int hashCode() {
+        return intField;
+    }
+
+    @Override
+    public String toString() {
+        return "" + intField;
+    }
+}
